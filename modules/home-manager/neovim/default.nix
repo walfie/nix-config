919f2338483027415372e2f4b7d0efdeb73a7362
Walfie <walfington@gmail.com>
2020-06-05 21:49:50 -0400

    Move neovim config into module

---
+++ modules/home-manager/neovim/default.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, lib, ... }:
+{
+  primary-user.home-manager.programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    configure.customRC = lib.fileContents ./vimrc;
+    configure.packages.neovim-with-plugins = with pkgs.vimPlugins; {
+      start = [
+        camelcasemotion
+        delimitMate
+        emmet-vim
+        fzf-vim
+        fzfWrapper
+        nerdcommenter
+        nerdtree
+        rainbow
+        traces-vim
+        vim-abolish
+        vim-eunuch
+        vim-nerdtree-tabs
+        vim-polyglot
+        vim-repeat
+        vim-sleuth
+        vim-surround
+        vim-visual-increment
+        zenburn
+
+        # TODO:
+        # dyng/ctrlsf.vim
+        # foosoft/vim-argwrap
+        # moll/vim-bbye
+        # weynhamz/vim-plugin-minibufexpl
+      ];
+
+      opt = [
+        vim-scala
+      ];
+    };
+  };
+}

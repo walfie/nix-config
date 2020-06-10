e247adff28cfbeffd2bab0a586c5dce53ee15d88
Walfie <walfington@gmail.com>
2020-06-09 20:21:52 -0400

    Add some more xdg directory overrides

--- modules/home-manager/xdg/default.nix
+++ modules/home-manager/xdg/default.nix
@@ -10,12 +10,24 @@
     dataFile."vim/undo/.keep".text = "";
     dataFile."vim/swap/.keep".text = "";
     dataFile."vim/backup/.keep".text = "";
+
+    configFile."npm/npmrc".text = ''
+      prefix=''${XDG_DATA_HOME}/npm
+      cache=''${XDG_CACHE_HOME}/npm
+      tmp=''${XDG_RUNTIME_DIR}/npm
+      init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
+    '';
   };
 
   primary-user.home-manager.home.sessionVariables = {
-    RLWRAP_HOME = "$XDG_DATA_HOME/rlwrap";
+    CARGO_HOME = "$XDG_DATA_HOME/cargo";
+    DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
+    INPUTRC = "$XDG_CONFIG_HOME/readline/inputrc";
     LESSHISTFILE = "$XDG_DATA_HOME/less/history";
     LESSKEY = "$XDG_DATA_HOME/less/lesskey";
+    NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/npmrc";
+    RLWRAP_HOME = "$XDG_DATA_HOME/rlwrap";
+    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
     WGETRC = "$XDG_CONFIG_HOME/wgetrc";
   };
 }

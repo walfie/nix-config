fb96d524b78d227460b2cb8ad6d1fb7326472cfb
Walfie <walfington@gmail.com>
2021-03-22 12:27:32 -0400

    Use `nixpkgs-fmt .`
    No need to use `find` since `nixpkgs-fmt .` finds the relevant files

--- shell.nix
+++ shell.nix
@@ -1,17 +1,10 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-
-  scripts = {
-    format = pkgs.writeShellScriptBin "format" ''
-      export PATH=${pkgs.lib.makeBinPath [ pkgs.nixpkgs-fmt ]}:$PATH
-      nixpkgs-fmt $(find . -name '*.nix')
-    '';
-  };
 in
 pkgs.mkShell {
   buildInputs = [
-    scripts.format
     pkgs.home-manager
+    pkgs.nixpkgs-fmt
   ];
 }

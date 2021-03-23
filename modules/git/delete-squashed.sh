6cf7829652784fdb346331d9125e234e4e3ba49d
Walfie <walfington@gmail.com>
2021-03-23 16:02:27 -0400

    Fix git-delete-squashed script

--- modules/git/delete-squashed.sh
+++ modules/git/delete-squashed.sh
@@ -1,11 +1,10 @@
 # https://github.com/not-an-aardvark/git-delete-squashed
-set -e
 git checkout -q master
 git for-each-ref refs/heads/ "--format=%(refname:short)" | \
-  while read branch
+  while read branch;
   do
-    mergeBase=$(git merge-base master $branch)
-    [[ $(git cherry master $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]]
-    git branch -D $branch
+    mergeBase=$(git merge-base master $branch) \
+      && [[ $(git cherry master $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] \
+      && git branch -D $branch;
   done
 

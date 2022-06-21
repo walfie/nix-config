5125a36dc8d266d69f65bc4729005c657b6ba104
Walfie <walfington@gmail.com>
2022-06-20 21:58:52 -0400

    Update git aliases to look up branch/remote names
    Instead of assuming the default branch is `master`, check to see if
    `main` exists first. The remote name is also checked, and prioritized in
    the order `upstream` and then `origin` and finally `walfie`.

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -3,6 +3,8 @@ let
   # Find and delete branches that were squash-merged
   git-delete-squashed =
     pkgs.writeShellScriptBin "git-delete-squashed" (lib.fileContents ./delete-squashed.sh);
+
+  mkGitScript = script: "!f() { ${script} }; f";
 in
 {
   home.packages = [
@@ -13,14 +15,27 @@ in
     enable = true;
 
     aliases = {
-      # Pull upstream/master branch
-      up = "!f() { local branch=\${1:-master}; git checkout $branch && git pull \${3:-upstream} $branch; }; f";
-
       # Squash changes into the previous commit
       fixup = "!git add -u && git commit --fixup=HEAD && git rebase -i --autosquash HEAD~2";
 
+      # Get default branch name (either `main` or `master`)
+      default-branch = "!git branch --format='%(refname:short)' | grep -m1 -E '^main|master$'";
+
+      # Get upstream name
+      upstream = "!git remote show | grep -m1 -E '^upstream|origin|walfie$'";
+
+      # Pull upstream/master branch
+      up = mkGitScript ''
+        local branch=$(git default-branch)
+        git checkout $branch && git pull $(git upstream) $branch
+      '';
+
       # git checkout (with fzf)
-      cof = "!f() { git for-each-ref --format='%(refname:short)' refs/heads | ${pkgs.fzf}/bin/fzf | xargs git checkout; }; f";
+      cof = mkGitScript ''
+        git branch --format='%(refname:short)' |
+        ${pkgs.fzf}/bin/fzf |
+        xargs git checkout
+      '';
     };
 
     extraConfig = {
--- modules/git/delete-squashed.sh
+++ modules/git/delete-squashed.sh
@@ -1,10 +1,14 @@
-# https://github.com/not-an-aardvark/git-delete-squashed
-git checkout -q master
-git for-each-ref refs/heads/ "--format=%(refname:short)" | \
+# This script assumes we have a git alias `default-branch`.
+#
+# Based on: https://github.com/not-an-aardvark/git-delete-squashed
+
+default_branch=$(git default-branch)
+git checkout -q $branch
+git branch --format='%(refname:short)' | \
   while read branch;
   do
-    mergeBase=$(git merge-base master $branch) \
-      && [[ $(git cherry master $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] \
+    mergeBase=$(git merge-base $default_branch $branch) \
+      && [[ $(git cherry $default_branch $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] \
       && git branch -D $branch;
   done
 

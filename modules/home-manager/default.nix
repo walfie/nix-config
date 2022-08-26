53ec5e1849a4643376c897627cef142e0f6193a3
Walfie <walfington@gmail.com>
2022-08-25 23:48:57 -0400

    Remove home-manager override

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,10 +2,6 @@
 let
   sources = import ../../nix/sources.nix;
 
-  home-manager = pkgs.home-manager.overrideAttrs (old: {
-    src = sources.home-manager;
-  });
-
   # Use pinned nixpkgs instead of channels
   nixPath = builtins.concatStringsSep ":" [
     "nixpkgs=${sources.nixpkgs}"
@@ -21,6 +17,6 @@ in
   };
 
   home.packages = [
-    home-manager
+    pkgs.home-manager
   ];
 }
--- shell.nix
+++ shell.nix
@@ -1,14 +1,10 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-
-  home-manager = pkgs.home-manager.overrideAttrs (old: {
-    src = sources.home-manager;
-  });
 in
 pkgs.mkShell {
   buildInputs = [
-    home-manager
+    pkgs.home-manager
     pkgs.nixpkgs-fmt
   ];
 }

de1d770a74e7c613754c34ba351393cf8420af80
Walfie <walfington@gmail.com>
2022-08-30 01:53:19 -0400

    Move rust-overlay to use flakes

--- flake.nix
+++ flake.nix
@@ -3,16 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ { nixpkgs, home-manager, ... }:
+  outputs = inputs @ { nixpkgs, home-manager, rust-overlay, ... }:
     let
       system = "x86_64-darwin";
-      pkgs = import nixpkgs { inherit system; };
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
     in
     {
       homeConfigurations.luminas = home-manager.lib.homeManagerConfiguration (

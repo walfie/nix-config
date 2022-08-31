bb797fc7cf959cb1d0f2168d5c0ebb4c73267123
Walfie <walfington@gmail.com>
2022-08-30 22:20:25 -0400

    Move vim plugins to nix flakes

--- flake.nix
+++ flake.nix
@@ -2,21 +2,40 @@
   description = "Home Manager configuration";
 
   inputs = {
+    # Flakes
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Vim plugins
+    vim-ctrlsf.url = "github:dyng/ctrlsf.vim";
+    vim-ctrlsf.flake = false;
+    vim-minibufexpl.url = "github:weynhamz/vim-plugin-minibufexpl";
+    vim-minibufexpl.flake = false;
   };
 
   outputs = inputs @ { nixpkgs, home-manager, rust-overlay, ... }:
     let
       system = "x86_64-darwin";
-      overlays = [ (import rust-overlay) ];
+
+      mkVimOverlay = { inputs, names }: final: prev:
+        let
+          mkVimPlugin = name: prev.vimUtils.buildVimPlugin {
+            inherit name;
+            src = inputs.${name};
+          };
+        in
+        {
+          vimPlugins = prev.vimPlugins // (prev.lib.genAttrs names mkVimPlugin);
+        };
+
+      vimOverlay = mkVimOverlay {
+        inherit inputs;
+        names = [ "vim-ctrlsf" "vim-minibufexpl" ];
+      };
+      overlays = [ (import rust-overlay) (vimOverlay) ];
       pkgs = import nixpkgs { inherit system overlays; };
     in
     {

487ae2e15146a364620acc1b1898842baed2e36d
Walfie <walfington@gmail.com>
2022-09-05 23:11:03 -0400

    Add some keymaps recommended by nvim-lspconfig

--- modules/neovim/lua/fzf-lua.lua
+++ modules/neovim/lua/fzf-lua.lua
@@ -22,18 +22,32 @@ vim.keymap.set("n", "<Leader>d", function()
   })
 end)
 
+local function with_preview(f)
+  return function()
+    f({
+      winopts = {
+        preview = {
+          hidden = "nohidden",
+          layout = "vertical",
+          vertical = "up:90%",
+        }
+      }
+    })
+  end
+end
+
 local lsp_actions = {
-  ["Type definitions"] = fzf.lsp_typedefs,
+  ["Type definitions"] = with_preview(fzf.lsp_typedefs),
   ["Signature help"] = vim.lsp.buf.signature_help,
   ["Rename"] = vim.lsp.buf.rename,
-  ["References"] = fzf.lsp_references,
+  ["References"] = with_preview(fzf.lsp_references),
   ["Outgoing calls"] = fzf.lsp_outgoing_calls,
   ["Incoming calls"] = fzf.lsp_incoming_calls,
-  ["Implementations"] = fzf.lsp_implementations,
+  ["Implementations"] = with_preview(fzf.lsp_implementations),
   ["Hover"] = vim.lsp.buf.hover,
   ["Formatting"] = vim.lsp.buf.formatting,
-  ["Definitions"] = fzf.lsp_definitions,
-  ["Declarations"] = fzf.lsp_declarations,
+  ["Definitions"] = with_preview(fzf.lsp_definitions),
+  ["Declarations"] = with_preview(fzf.lsp_declarations),
   ["Code actions"] = fzf.lsp_code_actions,
 }
 
@@ -65,4 +79,24 @@ vim.keymap.set("i", "<C-x><C-l>", function()
     },
   })
 end)
+
+-- In later versions of neovim we can use "LspAttach".
+-- E.g., `vim.api.nvim_create_autocmd("LspAttach", { callback = ... })`
+do
+  function nmap(keys, fn, desc)
+    vim.keymap.set("n", keys, fn, { desc = desc })
+  end
+
+  -- Set keymaps based on some recommendations from `nvim-lspconfig`
+  nmap("gD", with_preview(fzf.lsp_declarations), "LSP declarations")
+  nmap("gd", with_preview(fzf.lsp_definitions), "LSP definitions")
+  nmap("gi", with_preview(fzf.lsp_implementations), "LSP implementations")
+  nmap("K", vim.lsp.buf.hover, "LSP hover")
+  nmap("<C-k>", vim.lsp.buf.signature_help, "LSP signature help")
+  nmap("gr", with_preview(fzf.lsp_references), "LSP references")
+  nmap("<Space>D", with_preview(fzf.lsp_typedefs), "LSP type definition")
+  nmap("<Space>rn", vim.lsp.buf.rename, "LSP rename")
+  nmap("<Space>ca", fzf.lsp_code_actions, "LSP code action")
+  nmap("<Space><Space>", "<Space>")
+end
 

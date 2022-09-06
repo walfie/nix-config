1bf2a737b563ee6a6d5213a0b26d222555a99ae1
Walfie <walfington@gmail.com>
2022-09-06 00:34:52 -0400

    Deduplicate definitions for LSP mappings/options

--- modules/neovim/lua/fzf-lua.lua
+++ modules/neovim/lua/fzf-lua.lua
@@ -30,7 +30,7 @@ end)
 -- In later versions of neovim we can use "LspAttach".
 -- E.g., `vim.api.nvim_create_autocmd("LspAttach", { callback = ... })`
 do
-  function nmap(keys, fn, desc)
+  local function nmap(keys, fn, desc)
     vim.keymap.set("n", keys, fn, { desc = desc })
   end
 
@@ -45,14 +45,17 @@ do
     })
   end, "Find definition")
 
-  local function with_preview(f, size)
-    local size = size or 90
-    opts = {
+  -- There are other mappings with the <Space> prefix, so we should provide a
+  -- way to use default <Space>
+  vim.keymap.set("n", "<Space><Space>", "<Space>")
+
+  local function preview(f, size)
+    local opts = {
       winopts = {
         preview = {
           hidden = "nohidden",
           layout = "vertical",
-          vertical = "up:" .. size .. "%",
+          vertical = "up:" .. (size or 90) .. "%",
         }
       }
     }
@@ -62,49 +65,48 @@ do
     end
   end
 
-  local lsp_actions = {
-    ["Workplace symbols"] = fzf.lsp_workplace_symbols,
-    ["Workplace diagnostics"] = fzf.diagnostics_workspace,
-    ["Type definitions"] = with_preview(fzf.lsp_typedefs),
-    ["Signature help"] = vim.lsp.buf.signature_help,
-    ["Rename"] = vim.lsp.buf.rename,
-    ["References"] = with_preview(fzf.lsp_references),
-    ["Outgoing calls"] = fzf.lsp_outgoing_calls,
-    ["Incoming calls"] = fzf.lsp_incoming_calls,
-    ["Implementations"] = with_preview(fzf.lsp_implementations),
-    ["Hover"] = vim.lsp.buf.hover,
-    ["Formatting"] = vim.lsp.buf.formatting,
-    ["Document diagnostics"] = fzf.document_diagnostics,
-    ["Document symbols"] = fzf.lsp_document_symbols,
-    ["Definitions"] = with_preview(fzf.lsp_definitions),
-    ["Declarations"] = with_preview(fzf.lsp_declarations),
-    ["Code actions"] = fzf.lsp_code_actions,
+  local actions = {
+    { desc = "Code actions", fn = fzf.lsp_code_actions, nmap = "<Space>ca" },
+    { desc = "Declarations", fn = preview(fzf.lsp_declarations), nmap = "gD" },
+    { desc = "Definitions", fn = preview(fzf.lsp_definitions), nmap = "gd" },
+    { desc = "Document diagnostics", fn = fzf.document_diagnostics },
+    { desc = "Document symbols", fn = fzf.lsp_document_symbols },
+    { desc = "Formatting", fn = vim.lsp.buf.formatting },
+    { desc = "Hover", fn = vim.lsp.buf.hover, nmap = "K" },
+    { desc = "Implementations", fn = preview(fzf.lsp_implementations, 70), nmap = "gi" },
+    { desc = "Incoming calls", fn = fzf.lsp_incoming_calls },
+    { desc = "Outgoing calls", fn = fzf.lsp_outgoing_calls },
+    { desc = "References", fn = preview(fzf.lsp_references), nmap = "gr" },
+    { desc = "Rename", fn = vim.lsp.buf.rename, nmap = "<Space>rn" },
+    { desc = "Signature help", fn = vim.lsp.buf.signature_help, nmap = "<C-k>" },
+    { desc = "Type definitions", fn = preview(fzf.lsp_typedefs), nmap = "<Space>D" },
+    { desc = "Workplace diagnostics", fn = fzf.diagnostics_workspace },
+    { desc = "Workplace symbols", fn = fzf.lsp_workplace_symbols },
   }
 
+  local action_descriptions = {}
+  local action_to_fn = {}
+  for i, value in ipairs(actions) do
+    if (value.nmap ~= nil) then
+      nmap(value.nmap, value.fn, value.desc .. " (LSP)")
+    end
+
+    table.insert(action_descriptions, value.desc)
+    action_to_fn[value.desc] = value.fn
+  end
+
   -- Fuzzy finder for common LSP actions
-  nmap("n", "gl", function()
-    fzf.fzf_exec(vim.tbl_keys(lsp_actions), {
+  nmap("gl", function()
+    fzf.fzf_exec(action_descriptions, {
       prompt = "LSP> ",
       actions = {
         ["default"] = function(selected)
           for i, value in ipairs(selected) do
-            lsp_actions[value]()
+            action_to_fn[value]()
           end
         end,
       },
     })
   end, "LSP actions")
-
-  -- Set keymaps based on some recommendations from `nvim-lspconfig`
-  nmap("gD", with_preview(fzf.lsp_declarations), "LSP declarations")
-  nmap("gd", with_preview(fzf.lsp_definitions), "LSP definitions")
-  nmap("gi", with_preview(fzf.lsp_implementations), "LSP implementations")
-  nmap("K", vim.lsp.buf.hover, "LSP hover")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "LSP signature help")
-  nmap("gr", with_preview(fzf.lsp_references), "LSP references")
-  nmap("<Space>D", with_preview(fzf.lsp_typedefs), "LSP type definition")
-  nmap("<Space>rn", vim.lsp.buf.rename, "LSP rename")
-  nmap("<Space>ca", fzf.lsp_code_actions, "LSP code action")
-  nmap("<Space><Space>", "<Space>")
 end
 

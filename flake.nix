f4459c17a03868b1c5163388b209046b394fb3bf
Walfie <walfington@gmail.com>
2022-09-06 22:16:21 -0400

    Use workaround for installing cargo-watch on M1 Mac

--- flake.nix
+++ flake.nix
@@ -13,16 +13,26 @@
 
   outputs = inputs @ { nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-darwin";
+      # `cargo-watch` fails to build on M1 Mac, so use the x86 version.
+      #
+      # Workaround: https://github.com/hercules-ci/flake-parts/issues/50#issuecomment-1212175176
+      # Underlying issue: https://github.com/NixOS/nixpkgs/issues/146349
+      rosetta-overlay = final: prev: {
+        cargo-watch = inputs.nixpkgs.legacyPackages.x86_64-darwin.cargo-watch;
+      };
 
       overlays = [
         inputs.rust-overlay.overlays.default
         inputs.vim-plugins-overlay.overlays.default
+        rosetta-overlay
       ];
-      pkgs = import nixpkgs { inherit system overlays; };
     in
     {
       homeConfigurations.personal = home-manager.lib.homeManagerConfiguration (
+        let
+          system = "x86_64-darwin";
+          pkgs = import nixpkgs { inherit system overlays; };
+        in
         import ./machines/personal { inherit system pkgs; }
       );
     };

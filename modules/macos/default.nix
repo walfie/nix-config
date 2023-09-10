e77862735e87b0d6771ada99c1da9eaa429a4896
Walfie <walfington@gmail.com>
2023-09-10 00:03:25 -0400

    Source nix-daemon.sh in zsh init

--- modules/macos/default.nix
+++ modules/macos/default.nix
@@ -5,6 +5,15 @@
     ./applescript
   ];
 
+  # When applying macOS upgrades, `/etc/zshrc` changes that were made by the
+  # nix installer tend to be removed. To avoid having to update it every time,
+  # add it to user-specific zsh config. See https://github.com/NixOS/nix/issues/3616
+  programs.zsh.initExtra = ''
+    if [ -e "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh" ]; then
+      . "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
+    fi
+  '';
+
   home.activation.setDefaults =
     lib.hm.dag.entryAfter [ "writeBoundary" ] (lib.fileContents ./set-defaults.sh);
 

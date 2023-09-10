1af907f32128a36a3a508921250b2fc7957c6141
Walfie <walfington@gmail.com>
2023-09-10 17:52:40 -0400

    Remove outdated yarn wrapper

--- modules/javascript/default.nix
+++ modules/javascript/default.nix
@@ -1,28 +1,12 @@
 { config, pkgs, lib, ... }:
 let
-  yarnrc = pkgs.writeTextFile {
-    name = "yarnrc";
-    text = "disable-self-update-check true";
-  };
-
-  yarn = pkgs.symlinkJoin {
-    name = "yarn";
-    paths = [ pkgs.yarn ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/yarn \
-        --add-flags --use-yarnrc \
-        --add-flags ${yarnrc}
-    '';
-  };
-
   watchman = pkgs.callPackage ./watchman.nix { };
 in
 {
   home.packages = [
     pkgs.nodejs
     pkgs.nodePackages.pnpm
-    yarn
+    pkgs.yarn
 
     # This version of watchman seems to hang forever when called, so it's
     # commented out for now. Prefer installing it with the system's preferred

c181d3d5c2d91cbac2b4520db6fbd095b118c275
Walfie <walfington@gmail.com>
2024-06-19 22:27:16 -0400

    Set fish transient prompt

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -1,16 +1,5 @@
 { pkgs, lib, ... }:
 let
-  color = code: text: "(set_color --bold ${code})${text}";
-  space = "' '";
-  prompt = builtins.concatStringsSep "" [
-    "(date '+%H:%M:%S')"
-    space
-    (color "d78787" "(string replace $HOME '~' $PWD | path basename)")
-    (color "d7d7af" "❯")
-    "(set_color normal)"
-    space
-  ];
-
   mkPlugin = name: { src, ... }: { inherit name src; };
   mkPlugins = lib.attrsets.mapAttrsToList mkPlugin;
 in
@@ -22,26 +11,7 @@ in
       nix-fish = pkgs.fishExtraPlugins.nix-fish;
       replay = pkgs.fishExtraPlugins.replay;
       humantime-fish = pkgs.fishPlugins.humantime-fish;
-    };
-
-    functions = {
-      fish_greeting = "";
-      fish_prompt = "string join '' -- ${prompt}";
-      fish_right_prompt = ''
-        set -l last_status $status
-        set -q last_status_generation; or set -g last_status_generation $status_generation
-        if test $last_status_generation != $status_generation
-          if test $CMD_DURATION -gt 100
-            echo -n (humantime $CMD_DURATION)
-          end
-
-          if test $last_status -ne 0
-              echo -n (set_color red)" ✘ $last_status"
-          end
-        end
-        set last_status_generation $status_generation
-        set color normal
-      '';
+      transient-fish = pkgs.fishPlugins.transient-fish;
     };
 
     shellAliases = {
@@ -52,6 +22,49 @@ in
       ls = "${pkgs.coreutils}/bin/ls --color -h";
       man = "${pkgs.bat-extras.batman}/bin/batman";
       sed = "${pkgs.coreutils}/bin/sed";
+    };
+
+    interactiveShellInit = ''
+      set -g color_primary d78787
+      set -g color_secondary 87d7ff
+      set -g color_accent d7d7af
+      set -g color_dull 777777
+    '';
+
+    functions = {
+      fish_greeting = "";
+
+      fish_prompt = ''
+        echo -n \n(set_color $color_dull)(dirs | string replace -r '[^/]+$' (set_color --bold $color_primary)'$0')(set_color normal)
+
+        if test $transient_status -ne 0
+            echo -n (set_color $color_dull)" • "(set_color red)"✘ $transient_status"
+        end
+
+        if test $CMD_DURATION -gt 100
+            echo -n (set_color $color_dull)" • "(set_color $color_secondary)"⧖ "(humantime $CMD_DURATION)
+        end
+
+        echo \n(set_color $color_accent)"❯ "
+      '';
+
+      transient_prompt_func = ''
+        echo -n (date '+%H:%M:%S') (set_color --bold $color_primary)(pwd | path basename)(set_color $color_accent)"❯ "(set_color normal)
+      '';
+
+      transient_rprompt_func = ''
+        set -q last_status_generation; or set -g last_status_generation $status_generation
+        if test $last_status_generation != $status_generation
+            if test $CMD_DURATION -gt 100
+                echo -n "⧖ "(humantime $CMD_DURATION)
+            end
+
+            if test $transient_status -ne 0
+                echo -n (set_color red)" ✘ $transient_status"
+            end
+        end
+        set last_status_generation $status_generation
+      '';
     };
   };
 }
